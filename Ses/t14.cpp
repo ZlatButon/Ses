4f1afc341a4ff2aee4da340aff93bcdd36845000
Igor <42006386+QuinxiZz@users.noreply.github.com>
2019-11-24 22:14:47 +0300

    Update t14.cpp

--- Ses/t14.cpp
+++ Ses/t14.cpp
@@ -1,9 +1,19 @@
 //Даны шесть различных натуральных чисел. Определить максимальное из них (использовать функцию нахождения максимума из двух).
 #include <iostream>
+#include <math.h>
+#include <algorithm>
 
 using namespace std;
 
 void task14()
 {
+	int max;
+	int a = 1;
+	int b = 2;
+	int c = 3;
+	int d = 4;
+	int e = 5;
+	int f = 6;
 
+	
 }
