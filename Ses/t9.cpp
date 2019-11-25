e1c69be178cf6f5cd996dd8bd31c30286dd1672d
Igor <42006386+QuinxiZz@users.noreply.github.com>
2019-11-25 21:27:07 +0300

    Update t9.cpp

--- Ses/t9.cpp
+++ Ses/t9.cpp
@@ -5,5 +5,17 @@ using namespace std;
 
 void task9()
 {
+	int n;
+	int decision;
+	int i = 1;
 
+	cout << "¬ведите число" << endl;
+	cin >> n;
+
+	while (i * i <= n)
+	{
+		i++;
+	}
+	decision = i * i;
+	cout << "ѕервое число больше указанных = " << decision << endl;
 }
