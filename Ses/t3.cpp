f518663056ac449ad7f487b5fcb029c71ad21452
Igor <42006386+QuinxiZz@users.noreply.github.com>
2019-12-07 19:30:20 +0300

    Update t3.cpp

--- Ses/t3.cpp
+++ Ses/t3.cpp
@@ -1,22 +1,21 @@
-//Найти сумму - 12 + 22 – 32 + 42 – 5 2 + . . . + 402 . (Условный оператор не использовать)
+//-
 #include <iostream>
 
 using namespace std;
-
 void task3()
 {
 	int ArrayA[39] = { 0 };
 	int ArrayB[39] = { 0 };
 	int ArrayC[39] = { 0 };
 
-	cout << "Массив А" << endl;
+	cout << "Array A" << endl;
 	for (int i = 12; i < 39; i)
 	{
 		ArrayA[i] += 10;
 		cout << ArrayA[i] << endl;
 	}
 
-	cout << "Массив B" << endl;
+	cout << "Array B" << endl;
 	for (int i = -2; i < 39; i)
 	{
 		ArrayB[i] -= 10;
