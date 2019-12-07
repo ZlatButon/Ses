33a828c1b088c1d12bc23a11e01d198830b7b3cf
Igor <42006386+QuinxiZz@users.noreply.github.com>
2019-12-07 23:02:16 +0300

    Update t11.cpp

--- Ses/t11.cpp
+++ Ses/t11.cpp
@@ -5,5 +5,17 @@ using namespace std;
 
 void task11()
 {
-
+	int i, n, j;
+	double s = { 0 }, s1;
+	
+	cout << "Enter n" << endl;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		s1 = { 1 };
+		for (int j = 0; j < i; j++)
+			s1 = s1 * j;
+		s = s + s1;
+	}
+	cout << "s = " << s << endl;
 }
