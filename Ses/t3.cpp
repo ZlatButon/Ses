699a2730d0d1f576025d3c9d2882b39d36dafc35
Igor <42006386+QuinxiZz@users.noreply.github.com>
2019-12-07 22:38:46 +0300

    Update t3.cpp

--- Ses/t3.cpp
+++ Ses/t3.cpp
@@ -2,15 +2,13 @@
 #include <iostream>
 
 using namespace std;
-void task3()
-{
+void task3() {
 	int ArrayA[39] = { 0 };
 	int ArrayB[39] = { 0 };
 	int ArrayC[39] = { 0 };
 
 	cout << "Array A" << endl;
-	for (int i = 12; i < 39; i)
-	{
+	for (int i = 12; i < 39; i)	{
 		ArrayA[i] += 10;
 		cout << ArrayA[i] << endl;
 	}
