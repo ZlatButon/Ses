6e580952cb502e90f45c14f13ce20e9f9695b96a
Igor <42006386+QuinxiZz@users.noreply.github.com>
2019-12-07 22:38:47 +0300

    Update t6.cpp

--- Ses/t6.cpp
+++ Ses/t6.cpp
@@ -2,20 +2,17 @@
 
 using namespace std;
 
-void task6()
-{
+void task6() {
 	double a, value;
 	double n = 0;
 
 	cout << "Enter number a" << endl;
 	cin >> a;
 
-	for (int i = 1; i < 4; i++)
-	{
+	for (int i = 1; i < 4; i++) {
 		value = i + 1 / n;
 
-		if (n < a)
-		{
+		if (n < a) {
 			cout << value;
 		}
 	}
