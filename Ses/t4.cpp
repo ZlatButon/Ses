46dd6459dd52af8e99fe2b5ecef2c42df7e15a8e
Igor <42006386+QuinxiZz@users.noreply.github.com>
2019-12-07 22:38:43 +0300

    Update t4.cpp

--- Ses/t4.cpp
+++ Ses/t4.cpp
@@ -2,27 +2,22 @@
 
 using namespace std;
 
-void task4()
-{
+void task4() {
 	int n;
 
 	cout << "Enter n" << endl;
 	cin >> n;
 
-	while (n < 100 || n > 999)
-	{
+	while (n < 100 || n > 999) {
 		cout << "this is not a three digit number" << endl;
 		cin >> n;
 	}
 
-	for (int i = 100; i <= 999; i++)
-	{
-		if (i % n == 0)
-		{
+	for (int i = 100; i <= 999; i++) {
+		if (i % n == 0) {
 			cout << "number " << i << " divided by " << n << endl;
 		}
-		else
-		{
+		else {
 			cout << "number " << i << " not divided by " << n << endl;
 		}
 	}
