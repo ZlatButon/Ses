2ee70db816bcab84490ad4ee143ddcad5d4e8752
Igor <42006386+QuinxiZz@users.noreply.github.com>
2019-12-07 19:30:10 +0300

    Update t14.cpp

--- Ses/t14.cpp
+++ Ses/t14.cpp
@@ -7,13 +7,15 @@ using namespace std;
 
 void task14()
 {
-	int max;
-	int a = 1;
-	int b = 2;
-	int c = 3;
-	int d = 4;
-	int e = 5;
-	int f = 6;
+	int a = { 1 };
+	int b = { 2 };
+	int c = { 3 };
+	int d = { 4 };
+	int e = { 5 };
+	int f = { 6 };
+	int max = { 0 };
 
-	
+
+
+	cout << max;
 }
