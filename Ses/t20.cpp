b1d2b217491f297834543c1b9a67a894f32b238d
Igor <42006386+QuinxiZz@users.noreply.github.com>
2019-12-14 20:55:19 +0300

    Update t20.cpp

--- Ses/t20.cpp
+++ Ses/t20.cpp
@@ -2,24 +2,21 @@
 
 using namespace std;
 
-void task20 ()
-{
+void task20 () {
 	int arr[6] = { 1,2,3,4,5,6 };
 
 	cout << "Input:" << endl;
 	for (int i = 0; i < 6; i++)
 		cout << " " << arr[i];
 
-	for (int i = 1; i < 6; i += 2)
-	{
+	for (int i = 1; i < 6; i += 2) {
 		int t = arr[i - 1];
 		arr[i - 1] = arr[i];
 		arr[i] = t;
 	}
 
 	cout << "\n\nOutput:" << endl;
-	for (int i = 0; i < 6; i++)
-	{
+	for (int i = 0; i < 6; i++) {
 		cout << " " << arr[i];
 		cout << endl;
 	}
